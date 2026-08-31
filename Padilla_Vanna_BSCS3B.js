79a2ca84a7b7ff63272c51785d9abad52690f851
ccis <padillavanna12@gmail.com>
2026-08-31 13:56:11 +0800

    This is my first commit

---
+++ Padilla_Vanna_BSCS3B.js
@@ -0,0 +1,42 @@
+// Variables
+let studentName = "Vanna";
+let age = 20;
+let course = "BSCS";
+
+// Arrays
+let subjects = ["Programming", "Database", "Networking"];
+let grades = [90, 85, 88];
+let activities = ["Quiz", "Project", "Exam"];
+
+// Conditional 1
+if (age >= 18) {
+  console.log(studentName + " is an adult.");
+}
+
+// Conditional 2
+if (course === "BSCS") {
+  console.log(studentName + " is taking Computer Science.");
+}
+
+// Conditional 3
+if (grades[0] >= 75) {
+  console.log("Programming: Passed");
+}
+
+// Loop 1 - Display subjects
+console.log("Subjects:");
+for (let i = 0; i < subjects.length; i++) {
+  console.log(subjects[i]);
+}
+
+// Loop 2 - Display grades
+console.log("Grades:");
+for (let i = 0; i < grades.length; i++) {
+  console.log(grades[i]);
+}
+
+// Loop 3 - Display activities
+console.log("Activities:");
+for (let i = 0; i < activities.length; i++) {
+  console.log(activities[i]);
+}
